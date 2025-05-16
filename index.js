83409f7ce6ec6c2f5bc28fffd630156cbf41cde0
Ariful <arifulmit17@gmail.com>
2025-05-16 22:29:51 +0600

    simple server update

--- index.js
+++ index.js
@@ -44,6 +44,22 @@ async function run() {
             res.send(result);
         })
 
+        app.put('/users/:id',async (req,res)=>{
+            
+            const id=req.params.id
+            const query={_id: new ObjectId(id)}
+            const user=req.body;
+            const updatedDoc={
+                $set:{
+                    name:user.name,
+                    email:user.email
+                }
+            }
+            const options={upsert:true};
+            const result=await usersCollection.updateOne(query,updatedDoc,options);
+            res.send(result);
+        })
+
         app.delete('/users/:id',async (req,res)=>{
             console.log(req.params);
             const id=req.params.id
